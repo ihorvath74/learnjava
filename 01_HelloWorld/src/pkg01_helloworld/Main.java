6fe44e78055fff83ed5b74533c0e5101a1ec5040
horvathi <horvathi@NB69.loxon.local>
2013-03-11 14:03:25 +0100

    First test

---
+++ 01_HelloWorld/src/pkg01_helloworld/Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg01_helloworld;
+
+/**
+ *
+ * @author horvathi
+ * https://github.com/ihorvath74/learnjava.git
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+}
